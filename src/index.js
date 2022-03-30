2b2836961b40309e6ac5b1016f75063a598ba0c6
tushargoyal812 <69681896+tushargoyal812@users.noreply.github.com>
2022-03-30 19:38:27 +0530

    authentication done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import {BrowserRouter} from 'react-router-dom'
+import { AuthProvider } from "./context/auth-context";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <AuthProvider>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
